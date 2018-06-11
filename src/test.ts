64a0b0af2953cbbe87af64ac0c6f9a3bb3762f94
Joshua Claxton <joshua.claxton@drivetime.com>
2018-06-11 14:01:30 -0700

    meow

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,10 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
+
+/*******
+ * 
+ * Curt Is Pretty
+ ********/
 import 'zone.js/dist/long-stack-trace-zone';
 import 'zone.js/dist/proxy.js';
 import 'zone.js/dist/sync-test';
